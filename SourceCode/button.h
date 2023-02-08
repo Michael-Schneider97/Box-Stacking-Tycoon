1a81d71fa7cd5f67274a9d64e3cd5e9526ef5b87
Michael-Schneider <Mschneider8@student.rccd,edu>
2023-02-07 16:49:03 -0800

    Began implementing a menu class to handle the main, stats, and control menus used ingame.

---
+++ SourceCode/button.h
@@ -0,0 +1,18 @@
+// the button class used by menus
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "gameObject.h"
+
+class Button : public GameObject
+{
+    protected:
+
+    public:
+        Button();
+};
+
+#endif
+
+// buttons
+// calculate rect for location
---
+++ SourceCode/menu.cpp
@@ -0,0 +1,8 @@
+// the menu class, which uses SDL2 to create a framework for a menu based system 
+
+#include "menu.h"
+
+Menu::Menu() : GameObject()
+{
+    
+}
