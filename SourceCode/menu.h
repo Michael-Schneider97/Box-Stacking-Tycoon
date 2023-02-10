b6d6fd7c5e7d02d0876f29620815ea8d02a2a5b1
Michael-Schneider <Mschneider8@student.rccd,edu>
2023-02-09 16:25:00 -0800

    Created menu class header file.

--- SourceCode/menu.h
+++ SourceCode/menu.h
@@ -1,1 +1,27 @@
 // header file for the menu class
+
+#ifndef MENU_H
+#define MENU_H
+
+#include "gameObject.h"
+#include "button.h"
+#include <vector>           // used to store the buttons
+
+class Menu : public GameObject
+{
+    protected:
+        std::vector<Button> buttonList;
+        int buttonSpacing;
+        int titleSpacing;
+
+    public:
+        Menu();
+        void draw();
+        void generateRectangles();
+};
+
+#endif
+
+// constructor that takes a rect as an argument
+// background is setup using game object class
+// redefined draw() function which draws all sub objects
