3c325e4d0b2e9942165c3dbbb037844303fb968e
Michael-Schneider <Mschneider8@student.rccd,edu>
2023-02-19 15:02:04 -0800

    Implemented color change on mouse over for buttons in button class.

--- SourceCode/button.cpp
+++ SourceCode/button.cpp
@@ -1,6 +1,24 @@
 #include "button.h"
 
-Button::Button() : 
+// default constructor
+Button::Button() : GameObject()
 {
 
 }
+
+// mouse handler. Sets color on mouse over, use this in your game loop
+void Button::mouseHandler(int mouseX, int mouseY)
+{
+    if(mouseOver(mouseX, mouseY))
+    {
+        // set color to be a brighter red
+        SDL_SetTextureColorMod(texture.get(), 255, 200, 200);
+    }
+    else 
+    {
+        // set color back to normal
+        SDL_SetTextureColorMod(texture.get(), 255, 255, 255);
+    }
+}
+
+// set click execution. allows some action to be executed without cluttering your gameloop
